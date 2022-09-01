ba6146aa319d865c36d4db00dcfc4a10852446fb
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-01 13:41:04 -0400

    Add test, MVC for items request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'The items API' do
   it 'returns a list of all items' do
-    create_list(:item, 10)
+    merchants = create_list(:merchant, 2)
+    merchant1 = merchants[0]
+    merchant2 = merchants[1]
+    merchant1_items = create_list(:item, 5, merchant_id: merchant1.id)
+    merchant2_items = create_list(:item, 4, merchant_id: merchant2.id)
 
     get '/api/v1/items'
 
@@ -10,5 +14,20 @@ RSpec.describe 'The items API' do
 
     response_body = JSON.parse(response.body, symbolize_names: true)
     items = response_body[:data]
+
+    expect(items.count).to eq(9)
+    items.each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
+        expect(item[:id].to_i).to be_an(Integer)
+
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes][:name]).to be_a(String)
+        expect(item[:attributes][:description]).to be_a(String)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+        expect(item[:attributes][:merchant_id]).to be_an(Integer)
+
+        expect(item[:attributes]).to_not have_key(:created_at)
+    end
   end
 end
